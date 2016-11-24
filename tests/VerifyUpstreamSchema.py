8fafc2be5b25b5b34e796a19dbaf968a1f9c00a7
Alan Castonguay <acastonguay@vmware.com>
2016-11-24 17:43:05 -0500

    Clean up RAML test, accept path to RAML filename

--- tests/VerifyUpstreamSchema.py
+++ tests/VerifyUpstreamSchema.py
@@ -3,102 +3,11 @@
 # Rough and dirty validation of upstream RAML+JsonSchema
 
 from __future__ import print_function
-import sys
 import logging
 import json
-import os
 import jsonschema
-import traceback
-
-
-d = None
-
-def test():
-    BASEDIR = "../../../strata/loginsight/components/webservice/docs/src/"
-
-    known_examples = os.listdir(os.path.join(BASEDIR,"examples"))
-    known_schemas = os.listdir(os.path.join(BASEDIR,"schema"))
-
-    # Validate Example against Schema
-    validate = [
-        ("users.id.get.response.json", "users.id.get.response.schema.json"),
-        ("users.get.response.json", "users.get.response.schema.json"),
-        ("ad.request.json", "ad.schema.json"),
-        ("ad.response.json", "ad.schema.json"),
-
-
-        #'adgroups.get.response.json', 'adgroups.type.domain.name.capabilities.get.response.json',
-         #'adgroups.type.domain.name.get.response.json', 'aggregated-events.get.response.basic.json',
-        # 'aggregated-events.get.response.json', 'aggregated-events.get.response.simple.json',
-        #('events.get.response.json', 'events.get.response.simple.json',
-
-        ('alert.history.record.get.response.json','alert.history.record.get.response.schema.json'),
-        ('alert.history.record.put.request.json','alert.history.record.put.request.schema.json'),
-        ('alert.get.response.json','alert.get.response.schema.json'),
-        ('alert.history.records.get.response.json','alert.history.records.get.response.schema.json'),
-        ('alerts.get.response.json','alerts.get.response.schema.json'),
-        ('deployment.new.post.request.json','deployment.new.post.request.schema.json'),
-        ('users.post.request.json','users.post.request.schema.json'),
-        ('users.post.response.json','users.post.response.schema.json'),
-
-
-        #'error.detailed.schema.json',
-        #'error.message.schema.json', 'events.get.response.schema.json', ,
-
-        ('events.get.response.simplewarnings.json', 'events.get.response.schema.json'),
-        ('events.get.response.simple.json', 'events.get.response.schema.json'),
-        ('events.get.response.json', 'events.get.response.schema.json'),
-
-        ('aggregated-events.get.response.json', 'aggregated-events.get.response.schema.json'),
-
-        ('aggregated-events.get.response.simple.json', 'aggregated-events.get.response.schema.json'),
-        ('aggregated-events.get.response.simplewarnings.json', 'aggregated-events.get.response.schema.json'),
-
-
-        ('events.ingest.post.request.json','events.ingest.post.request.schema.json'),
-        ('events.ingest.post.response.json', 'events.ingest.post.response.schema.json'),
-        ('deployment.join.post.request.json','deployment.join.post.request.schema.json'),
-        ('deployment.join.post.response.json', 'deployment.join.post.response.schema.json'),
-        ('deployment.approve.post.request.json', 'deployment.approve.post.request.schema.json'),
-        ('deployment.approve.post.response.json', 'deployment.approve.post.response.schema.json'),
-
-
-        ('error.deploymenterror.json', 'error.detailed.schema.json'),
-        ('error.ad.wrong_credentials.json', 'error.detailed.schema.json'),
-        ('error.deployment.not_boostrapped.json', 'error.message.schema.json'),
-        ('ad.response.json','ad.schema.json'),
-        ('ad.request.json','ad.schema.json'),
-
-        #('agent.status.get.response.json', 'agent.status.get.response.schema.json')
-
-    ]
-
-    for p in validate:
-        with open(os.path.join(BASEDIR,"examples", p[0]),'r') as f:
-            example = json.load(f)
-        with open(os.path.join(BASEDIR,"schema", p[1]), 'r') as f:
-            schema = json.load(f)
-
-        try:
-           jsonschema.validate(example, schema)
-        except:
-            sys.stderr.write("\n\n\nFailed to validate {0} against {1}\n".format(p[0],p[1]))
-            traceback.print_exc()
-            break
-
-        if p[0] in known_examples:
-            known_examples.remove(p[0])
-        if p[1] in known_schemas:
-            known_schemas.remove(p[1])
-
-    sys.stderr.write("\n\n\n=================\nSummary\n".format(p[0], p[1]))
-
-    sys.stderr.write("\nUntested examples:\n")
-    sys.stderr.write(str(known_examples))
-    sys.stderr.write("\nUntested schemas:\n")
-    sys.stderr.write(str(known_schemas))
-    sys.stderr.write("\n")
-
+import sys
+import argparse
 
 def test_example_against_schema(examplestring, schema, verb=None, path=None, ctx=None):
     """
@@ -181,19 +90,15 @@ def check_resources(resources, name=""):
 
 
 
-def check_raml_file():
-    BASEDIR = "/Users/acastonguay/workspace/strata/loginsight/components/webservice/docs/src/"
-
+def check_raml_file(ramlfile_path):
     import pyraml.parser
 
-    root = pyraml.parser.load(os.path.join(BASEDIR, "api.raml"))
+    root = pyraml.parser.load(ramlfile_path)
 
     check_resources(root.resources)
 
     return root
 
-
-    #r = ramlfications.parse()
 
 
 def parse_url_response(raml, verb, route, response, status=200, contenttype='application/json'):
@@ -207,11 +112,12 @@ if __name__ == "__main__":
     formatter = logging.Formatter(u'%(asctime)s %(levelname)s: %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-    
-    test()
 
-    p = check_raml_file()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
 
+    parsedramlroot = check_raml_file(args.filename)
 
     version = """{
                 "releaseName": "GA",
