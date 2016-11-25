a805e3824122c374bc72b14324dbef6837bc9e82
Alan Castonguay <acastonguay@vmware.com>
2016-11-24 21:38:22 -0500

    RAML Validation: skip 'is'-flagged or response codes
    Ignore any resource which "is" something (e.g., techPreview)
    Ignore any resource's response (e.g., 404)
    
    Bonus: pep8 (except E501 line length)

--- tests/VerifyUpstreamSchema.py
+++ tests/VerifyUpstreamSchema.py
@@ -12,34 +12,46 @@ import collections
 
 stats = collections.Counter()
 
+
 class TestValidationError(RuntimeError):
     def __init__(self, message, context, exception=None):
         super(RuntimeError, self).__init__(message)
         self.innerexception = exception
         self.context = context
 
-        stats[self.__class__.__name__]+=1
+        stats[self.__class__.__name__] += 1
 
     def __str__(self):
-        #if self.innerexception is None:
         return "%s: %s" % (self.__class__.__name__, self.context)
-        #else:
-        #    return "%s: %s:\n%s" % (self.__class__.__name__, self.context, self.innerexception)
 
-class EmptyBody(TestValidationError):
-    """The example or schema body was blank. It should be populated."""
+
+class Empty(TestValidationError):
+    """The example or schema was blank. It should be populated."""
+
+
+class EmptySchema(Empty):
+    """The example or schema was blank. It should be populated."""
+
+
+class EmptyExample(Empty):
+    """The example or schema was blank. It should be populated."""
+
 
 class SchemaValidationError(TestValidationError):
     """The schema was invalid."""
 
+
 class ExampleVsSchemaValidationError(TestValidationError):
     """The example was invalid according to the schema."""
+
 
 class Schema03(SchemaValidationError):
     """JSONSchema03 shouldn't be used."""
 
+
 class UnknownSchemaType(TestValidationError):
     """Schema isn't JSONSchema03 or JSONSchema04"""
+
 
 def test_example_against_schema(examplestring, schema, verb=None, path=None, ctx=None):
     """
@@ -50,17 +62,17 @@ def test_example_against_schema(examplestring, schema, verb=None, path=None, ctx
 
     try:
         if examplestring is None:
-            raise EmptyBody('Missing example.', context=context)
+            raise EmptyExample('Missing example.', context=context)
 
         if schema is None:
-            raise EmptyBody('Missing schema.', context=context)
+            raise EmptyExample('Missing schema.', context=context)
 
         try:
             example = json.loads(examplestring)
         except Exception as e:
             raise SchemaValidationError("Schema isn't valid JSON.", context=context, exception=e)
         if example == {}:
-            raise EmptyBody("Example = {}", context=context)
+            raise EmptyExample("Example = {}", context=context)
 
         if args.no3 and schema['$schema'] == 'http://json-schema.org/draft-03/schema':
             raise Schema03('', context=context)
@@ -73,8 +85,8 @@ def test_example_against_schema(examplestring, schema, verb=None, path=None, ctx
             try:
                 jsonschema.validate(example, schema)
                 if not args.quiet:
-                    logging.debug('Valid: context={}'.format(context))
-                    stats['Valid']+=1
+                    logging.debug('Valid: {}'.format(context))
+                    stats['Valid'] += 1
                 return True
 
             except jsonschema.exceptions.SchemaError as e:
@@ -86,12 +98,13 @@ def test_example_against_schema(examplestring, schema, verb=None, path=None, ctx
         else:
             raise UnknownSchemaType("Can't test schema {}".format(schema['$schema']), context=context)
 
-    except EmptyBody as e:
+    except Empty as e:
         if not args.ignoremissing:
             raise e
     except SchemaValidationError as e:
         if not args.ignoreinvalidschema:
             raise e
+
 
 def check_body(body, verb=None, path=None, ctx=None):
     """
@@ -100,14 +113,18 @@ def check_body(body, verb=None, path=None, ctx=None):
     :return:
     """
     if body is None:
-        #context = "{} {} {}".format(verb, path, ctx)
-        #print('Missing body, ignoring context={}'.format(context))
+        # A body isn't strictly required. For example, the server may respond with a HTTP 200 with no JSON
+        # logging.warning("MissingBody: {} {} {}".format(verb, path, ctx))
+        # stats['MissingBody'] += 1
         return
 
     for mime in body:
         critical = None
         try:
             test_example_against_schema(body[mime].example, body[mime].schema, verb, path, ctx)
+        except Empty as e:
+            logging.warning(str(e))
+            critical = e.innerexception  # unpack inner exception, such as a jsonschema.exceptions.SchemaError
         except TestValidationError as e:
             logging.error(str(e))
             critical = e.innerexception  # unpack inner exception, such as a jsonschema.exceptions.SchemaError
@@ -115,28 +132,44 @@ def check_body(body, verb=None, path=None, ctx=None):
             raise critical
 
 
+def skipis(resource, islist, name=""):
+    if resource.is_:
+        for isness in resource.is_:
+            if isness in islist:
+                # logging.debug("Ignoring %s for isness %s" % (name, isness))
+                return True
+        return False
+
+
 def check_resources(resources, name=""):
     for resource in resources:
-        #print("{} has methods {}".format(resource, resources[resource].methods))
+        isness = []
+
+        if args.skipis and skipis(resources[resource], args.skipis, name=(name + resource)):
+            stats['skipis'] += 1
+            continue
+
+        isness = resources[resource].is_ or []
+        if 'internal' not in isness and 'supported' not in isness and 'techPreview' not in isness:
+            isness.append("supported")
+        if 'authenticated' in isness:
+            isness.remove('authenticated')  # unimportant for logging
+
+        supportedResource = "[%s]" % ",".join(isness)
         if resources[resource].methods:
             for method in resources[resource].methods:
-
                 m = resources[resource].methods[method]
-
-                try:
-                    check_body(m.body, method, name+resource, "")
-                except:
-                    print("FAIL check {0} {1}{2}".format(method, name, resource))
-                    raise
+                if method != 'get':
+                    check_body(m.body, method, name + resource, supportedResource)
 
                 for response in m.responses:
-                    check_body(m.responses[response].body, method, name+resource, "response")
+                    if str(response) in args.skipresponse:
+                        stats['skipresponse'] += 1
+                        continue
+                    check_body(m.responses[response].body, method, name + resource, "response " + str(response) + " " + supportedResource)
 
         if resources[resource].resources:
-            check_resources(resources[resource].resources, name+resource)
-
-
-
+            check_resources(resources[resource].resources, name + resource)
 
 
 def check_raml_file(ramlfile_path):
@@ -149,17 +182,15 @@ def check_raml_file(ramlfile_path):
     return root
 
 
-
 def parse_url_response(raml, verb, route, response, status=200, contenttype='application/json'):
     routechunks = route.split('/')[1:]
 
     resource = raml
     for _ in routechunks:
-        resource = resource.resources['/'+_]
+        resource = resource.resources['/' + _]
 
     body = resource.methods[verb].responses[status].body[contenttype]
-    return test_example_against_schema(response, body.schema)
-
+    return test_example_against_schema(response, body.schema, verb=verb, path=route, ctx="example")
 
 
 if __name__ == "__main__":
@@ -180,7 +211,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-
     parser.add_argument("filename",
                         help="Existing RAML file to be read. Required.")
     parser.add_argument("--fastfail", action="store_true", default=False,
@@ -193,6 +223,10 @@ if __name__ == "__main__":
                         help="If the schema/example is Blank or {}, ignore it.")
     parser.add_argument("--no3", action="store_true", default=False,
                         help="Treat JSONSchema Draft 3 as an error.")
+    parser.add_argument("--skipis", action="append",
+                        help="ignore any resource which 'is' in this list; e.g., techPreview")
+    parser.add_argument("--skipresponse", action="append",
+                        help="ignore any resource response in this list; e.g., 404")
     # supported
 
     args = parser.parse_args()
