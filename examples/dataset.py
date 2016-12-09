879b47c595623f96646d906b23a2bf25584dbc03
Daniel de Sao Jose <dsaojose@vmware.com>
2016-12-09 17:04:59 -0500

    Looks like I had some duplicate classes, needed to remove some unused code.

--- examples/dataset.py
+++ examples/dataset.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import json
+
 class Dataset():
     """ Class used to interact with and existing dataset on the Log Insight server. """
 
@@ -17,33 +19,22 @@ class Dataset():
 
 class DatasetSpec():
     """ Class that defines a dataset that needs to be created. """
+    # TODO: This needs to support constraints.  We may want to add constraints
+    # to datasets as a separate operation.
    
-    def __init__(self, id="", name="", description="", constraints=[], type=""):
+    def __init__(self, name, description="", constraints=[]):
         self.name = name
         self.description = description
         self.constraints = constraints
 
-    def __repr__(self):
-        """A human-readable representation of the DatasetSpec, in constructor form."""
-        return '{cls}(name={x.name!r}, description={x.description!r}, constraints={x.constraints!r}'.format(cls=self.__class__.__name__, x=self)
-   
-
-class PostDatasetSpec():
-    """ Class that enforces the data being sent to LI when creating a dataset. """
-
-    def __init__(self, name, description, constraints):
-        self.name = name
-        self.description = description
-        self.constraints = constraints
-
-    def dict(self):
-        return {
+    def json(self):
+        return json.dumps({
             'name': self.name,
             'description': self.description,
             'constraints': self.constraints
-        }
+            })
 
     def __repr__(self):
-        """A human-readable representation of the constraint, in constructor form."""
-        return '{cls}(id={x.name!r}, description={x.description!r}, constraint_list={x.constraints})'.format(cls=self.__class__.__name__, x=self)
-
+        """A human-readable representation of the DatasetSpec, in constructor form."""
+        return '{cls}(name={x.name!r}, description={x.description!r})'.format(cls=self.__class__.__name__, x=self)
+   
