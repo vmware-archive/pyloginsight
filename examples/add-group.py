7af50fb2b1db72d8ea20919f71ce928277f08304
Daniel de Sao Jose <dsaojose@vmware.com>
2016-12-10 02:02:54 -0500

    Updated the import statements to reflect new models.py, and removed the add_group return value until I can think of something better.

--- examples/add-group.py
+++ examples/add-group.py
@@ -1,43 +1,44 @@
 #!/usr/bin/env python
 
-from pyloginsight.Connection import Connection, Server, Credentials
-from capability import Capability
-from group import Group, GroupSpec
+from pyloginsight.Connection import Server
+from models import GroupSpec
 import argparse
+import sys
 
 
 class ServerPlus(Server):
-    """ Extends the functionality of the Server class by adding groups, and
-    datasets. """
+    """ Extends the functionality of the Server class by adding groups, and datasets. """
 
-    def add_group(self):
-        """ Add a group to Log Insight. """
-        raise NotImplemented
+    def add_group(self, name, capabilities, description=''):
+        """ Given a Group class instance, create it on the server. """
+
+
+        known_capabilities = ('ANALYTICS', 'DASHBOARDS', 'EDIT_ADMIN', 'EDIT_SHARED', 'INTERNAL', 'INVENTORY', 'STATISTICS', 'VIEW_ADMIN')
+        data = GroupSpec(name=name, description=description, capabilities=capabilities).json()
+        response = server._post('/groups', data=data)
+
+        if not response.ok:
+            print(response.json()['errorMessage'])
+            if response.json()['errorDetails']['errorCode'] == 'com.vmware.loginsight.api.errors.rbac.wrong_capability_id_specified':
+                print('The --capabilities values must be ond of {l}'.format(l=known_capabilities))
+            sys.exit(1)
 
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--username', required=False, default=None)
-    parser.add_argument('-p', '--password', required=False, default=None)
-    parser.add_argument('-P', '--provider', required=False, default=None)
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('-P', '--provider', required=True)
     parser.add_argument('-s', '--server', required=True)
+    parser.add_argument('-n', '--name', required=True)
+    parser.add_argument('-d', '--description', default='')
+    parser.add_argument('-c', '--capabilities', required=True, nargs='+')
     args = parser.parse_args()
 
     server = ServerPlus(args.server, verify=False)
-
-
-    if not args.provider:
-        # TODO: Get a list of providers for the user and display them.
-        pass
-
-
-    if args.username and args.password and args.provider:
-        server.login(
-            username=args.username,
-            password=args.password,
-            provider=args.provider
-        )
+    server.login(username=args.username, password=args.password, provider=args.provider)
+    server.add_group(name=args.name, description=args.description, capabilities=args.capabilities)
 
 
