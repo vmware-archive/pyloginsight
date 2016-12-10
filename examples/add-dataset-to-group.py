2a4e11e08ba8d946d414bae848a85708933d807a
Daniel de Sao Jose <dsaojose@vmware.com>
2016-12-10 17:19:33 -0500

    Did some groundwork on the add_dataset_to_group function.  Started having problems wish JSON function of the Spec class.  Commit to work on later.

--- examples/add-dataset-to-group.py
+++ examples/add-dataset-to-group.py
@@ -2,15 +2,25 @@
 
 from pyloginsight.Connection import Server
 from pyloginsight.query import Constraint
+from models import PostGroupIdDatasetSpec
 import argparse
 
 
 class ServerPlus(Server):
-    """ Extends the functionality of the Server class by adding groups, and
-    datasets. """
+    """ Extends the functionality of the Server class by adding groups, and datasets. """
 
-    def add_dataset_to_group(self, dataset, group):
-        pass
+    def add_dataset_to_group(self, dataset_id, group_id):
+        
+        data = PostGroupIdDatasetSpec(
+            group_id = group_id,
+            dataset_add_list = [dataset_id],
+            dataset_remove_list = []
+        ).json()
+
+        response = self._patch('/groups/{group_id}/datasets'.format(group_id=group_id, data=data))
+
+        if not response.ok:
+            print(response.json())
 
 
 if __name__ == "__main__":
@@ -20,12 +30,12 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--password', required=True)
     parser.add_argument('-P', '--provider', required=True)
     parser.add_argument('-s', '--server', required=True)
-    parser.add_argument('-g', '--group', required=True)
-    parser.add_argument('-d', '--dataset', required=True)
+    parser.add_argument('-g', '--group_id', required=True)
+    parser.add_argument('-d', '--dataset_id', required=True)
     args = parser.parse_args()
 
     server = ServerPlus(args.server, verify=False)
     server.login( username=args.username, password=args.password, provider=args.provider)
-    server.add_dataset_to_group(group=args.group, dataset=args.dataset)
+    server.add_dataset_to_group(group_id=args.group_id, dataset_id=args.dataset_id)
 
 
