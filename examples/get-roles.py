8d7eb85696ed17b4eaec3e2a366eafb2aaa5864a
dsaojose <dsaojose@vmware.com>
2016-12-17 21:47:33 -0500

    Re-named the file to better reflect the operation that is done using UI terminology.  Also, re-wrote the example to leverage the Roles class and roles property that I introduced in earlier commits.

---
+++ examples/get-roles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from pyloginsight.models import Server, Credentials
+import argparse
+import pprint
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', required=True)
+    parser.add_argument('-p', '--password', required=True)
+    parser.add_argument('-P', '--provider', required=True)
+    parser.add_argument('-s', '--server', required=True)
+    args = parser.parse_args()
+
+    creds = Credentials(username=args.username, password=args.password, provider=args.provider)
+    server = Server(hostname=args.server, verify=False, auth=creds)
+
+    pprint.pprint({k:v for (k, v) in server.roles.items()})
