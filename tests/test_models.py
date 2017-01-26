3b983f486ebff1b54f98327b2dc473de08c5cb1e
dsaojose <dsaojose@vmware.com>
2017-01-26 15:15:58 -0500

    Wrote a convenience function that works similar to named tuple, but also defines the default values. and wrote tests to capture immutable data types we may return from the server such as strings and tuples.

---
+++ tests/test_models.py
@@ -0,0 +1,18 @@
+import pytest
+import pyloginsight.models
+
+
+def test_named_tuple_with_defaults():
+
+    ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields='field1 field2')
+    assert ThingClass() == ThingClass(field1=None, field2=None)
+
+    ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['field1', 'field2'])
+    assert ThingClass() == ThingClass(field1=None, field2=None)
+
+    ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['field1', 'field2'], values=['value1', 'value2'])
+    assert ThingClass() == ThingClass(field1='value1', field2='value2')
+
+    ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['tuple1', 'tuple2'], values=[(), ()])
+    assert ThingClass() == ThingClass(tuple1=(), tuple2=())
+
