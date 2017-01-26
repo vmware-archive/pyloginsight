91fdfcd3755af49d951f4d77bd4e4b46dfa003db
dsaojose <dsaojose@vmware.com>
2017-01-26 16:03:54 -0500

    Was only using classes on tests instead of instances.  Used instances of the resulting classes in each test.

--- tests/test_models.py
+++ tests/test_models.py
@@ -20,29 +20,45 @@ def test_namedtuple_with_good_paramters_bad_data():
 def test_namedtyple_with_only_fields_as_list():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['field1', 'field2'])
     assert ThingClass() == ThingClass(field1=None, field2=None)
-
+    thing = ThingClass()
+    assert thing.field1 is None
+    assert thing.field2 is None
 
 def test_namedtyple_with_only_fields_as_string():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields='field1 field2')
     assert ThingClass() == ThingClass(field1=None, field2=None)
-
+    thing = ThingClass()
+    assert thing.field1 is None
+    assert thing.field2 is None
 
 def test_namedtyple_with_values_as_list_of_strings():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['field1', 'field2'], values=['value1', 'value2'])
     assert ThingClass() == ThingClass(field1='value1', field2='value2')
-
+    thing = ThingClass()
+    assert thing.field1 == 'value1'
+    assert thing.field2 == 'value2'
 
 def test_namedtyple_with_values_as_list_of_empty_tuples():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['tuple1', 'tuple2'], values=[(), ()])
     assert ThingClass() == ThingClass(tuple1=(), tuple2=())
+    thing = ThingClass()
+    assert thing.tuple1 == ()
+    assert thing.tuple2 == ()
 
 
 def test_namedtyple_with_values_as_list_of_empty_strings():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['string1', 'string2'], values=['', ''])
     assert ThingClass() == ThingClass(string1='', string2='')
+    thing = ThingClass()
+    assert thing.string1 == ''
+    assert thing.string2 == ''
 
 
 def test_namedtyple_with_values_as_list_of_none():
     ThingClass = pyloginsight.models.named_tuple_with_defaults('ThingClass', fields=['none1', 'none2'], values=[None, None])
     assert ThingClass() == ThingClass(none1=None, none2=None)
+    thing = ThingClass()
+    assert thing.none1 is None
+    assert thing.none2 is None
+
 
