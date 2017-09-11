37419a71c8cf136cab72f8f0d0a5d4971a56164e
Alan Castonguay <acastonguay@vmware.com>
2017-09-11 15:16:57 -0400

    Add dependency on attrdict

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     url='http://github.com/vmware/pyloginsight/',
     license='Apache Software License 2.0',
     author='Alan Castonguay',
-    install_requires=['requests', 'marshmallow', 'six'],
+    install_requires=['requests', 'marshmallow', 'attrdict', 'six'],
     tests_require=["requests_mock", "pytest", "pytest-catchlog", "pytest-flakes", "pytest-pep8"],
     description='VMware vRealize Log Insight Client',
     author_email='acastonguay@vmware.com',
